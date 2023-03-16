a04083f2ccecfdbacf36ba47935b598be9c72899
ACER <ACER@BALLMAGE>
2023-03-16 17:52:36 +0300

    Changed Hello Word

--- FigureTestZad/Program.cs
+++ FigureTestZad/Program.cs
@@ -6,7 +6,7 @@ namespace FigureTestZad
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Привет Мир");
         }
     }
 }
